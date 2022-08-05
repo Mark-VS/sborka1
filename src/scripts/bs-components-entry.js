c3b3853d752326908c89cfc50faafcb28953b7bc
Mark-VS <mark-sw@yandex.ru>
2022-08-05 08:23:33 +0300

    Progress

--- src/scripts/bs-components-entry.js
+++ src/scripts/bs-components-entry.js
@@ -1,6 +1,11 @@
-//import $ from "jquery";
-//import { createPopper } from "@popperjs/core";
+import $ from "jquery";
+import { createPopper } from "@popperjs/core";
 import Collapse from "./../../node_modules/bootstrap/js/dist/collapse";
 import Dropdown from "./../../node_modules/bootstrap/js/dist/dropdown";
 import Modal from "./../../node_modules/bootstrap/js/dist/modal";
 import Offcanvas from "./../../node_modules/bootstrap/js/dist/offcanvas";
+import Popover from "./../../node_modules/bootstrap/js/dist/popover";
+
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
